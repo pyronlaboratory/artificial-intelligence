6ec25c1807907fbcaa6e27d782e1756b3105ed7c
Ronyx <ronnie94official@gmail.com>
2018-12-28 16:54:44 +0530

    updated-classifier

--- #2 tensorflow-scikit-learn-iris-flower/iris.py
+++ #2 tensorflow-scikit-learn-iris-flower/iris.py
@@ -1,10 +1,11 @@
-import tensorflow as tf
+import tensorflow as tf
+import tensorflow.contrib.learn as skflow
 from sklearn import datasets, metrics
 
 iris = datasets.load_iris()
-classifier = skflow.TensorFlowLinearClassifier(hidden_units=[10,20,10],n_classes=3)
 
-classifier.fit(iris.data, iris_target)
-score = metrics.accuracy_score(iris_target,classifier.predict(iris_data))
+classifier = skflow.LinearClassifier(feature_columns=[tf.contrib.layers.real_valued_column("", dimension=iris.data.shape[1])], n_classes=3)
+classifier.fit(iris.data, iris.target)
+score = metrics.accuracy_score(iris.target,classifier.predict(iris.data))
 
 print("Accuracy: %f" %score)
